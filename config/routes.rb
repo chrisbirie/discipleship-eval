2506fe09f0b13c390733145f2a8c144eeff809be
chrisbirie <chris.birie@gmail.com>
2017-04-04 23:03:25 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  resources :posts
+  root :to => 'home#index'
+  get 'controller/:id/See Messages', :to => 'posts#show'
+  get 'controller/:id/Write Messages', :to => 'posts#new'
+
+end
